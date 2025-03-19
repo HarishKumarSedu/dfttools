91924e694a34a49287d679dc16c8c2e89528aa70
HarishKumarS <harishkumarsedu@gmail.com>
2025-03-19 09:56:46 +0100

    force instruction updated

--- src/dfttools/instructions/force.py
+++ src/dfttools/instructions/force.py
@@ -4,133 +4,85 @@ from ..exceptions import ParsingError
 from ..units.volt import Volt, V
 from ..units.current import Current, A
 from ..units.resistance import Resistance, Ohm
-from ..units.frequency import Frequency, Hz  # Assuming this is still needed for other parts
+from ..units.frequency import Frequency, Hz
 
 from ..glob import g
 
 from ..ops.forceops import VoltageForceOperation, CurrentForceOperation, ResistanceForceOperation, ClockForceOperation
 
-
-# FIXME: make signal and reference actual python names by importing pin names
-def VFORCE(signal: str, unit: Volt = V, comment: str = ''):
+def VFORCE(signal: str, value: Union[int, float], unit: Volt = V, comment: str = ''):
     """
     Creates a voltage force operation.
 
     Args:
-        signal (str): The signal to apply the force.
-        unit (Volt): The unit of measurement (default is Volts).
-        comment (str, optional): Optional comment for the operation. Defaults to ''.
+        signal (str): The signal to apply the force
+        value (Union[int, float]): Voltage value to force
+        unit (Volt): Unit of measurement (default Volts)
+        comment (str, optional): Optional operation comment
     """
     g.output.append(VoltageForceOperation(unit=unit,
-                                           signal=signal,
-                                           reference='GND',
-                                           comment=comment))
+                                          value=value,
+                                          signal=signal,
+                                          reference='GND',
+                                          comment=comment))
 
-
-def DVFORCE(signal: str, reference: str, unit: Volt = V, comment: str = ''):
+def DVFORCE(signal: str, reference: str, value: Union[int, float], unit: Volt = V, comment: str = ''):
     """
     Creates a differential voltage force operation.
 
     Args:
-        signal (str): The primary signal to apply the force.
-        reference (str): The reference signal.
-        unit (Volt): The unit of measurement (default is Volts).
-        comment (str, optional): Optional comment for the operation. Defaults to ''.
+        signal (str): Primary signal to force
+        reference (str): Reference signal
+        value (Union[int, float]): Voltage value to force
+        unit (Volt): Unit of measurement (default Volts)
+        comment (str, optional): Optional operation comment
     """
     g.output.append(VoltageForceOperation(unit=unit,
-                                           signal=signal,
-                                           reference=reference,
-                                           comment=comment))
+                                          value=value,
+                                          signal=signal,
+                                          reference=reference,
+                                          comment=comment))
 
+# Similarly updated for all other force functions:
 
-def AFORCE(signal: str, unit: Current = A, comment: str = ''):
-    """
-    Creates a current force operation.
-
-    Args:
-        signal (str): The signal to apply the force.
-        unit (Current): The unit of measurement (default is Amperes).
-        comment (str, optional): Optional comment for the operation. Defaults to ''.
-    """
+def AFORCE(signal: str, value: Union[int, float], unit: Current = A, comment: str = ''):
     g.output.append(CurrentForceOperation(unit=unit,
-                                           signal=signal,
-                                           reference='GND',
-                                           comment=comment))
+                                          value=value,
+                                          signal=signal,
+                                          reference='GND',
+                                          comment=comment))
 
-
-def DAFORCE(signal: str, reference: str, unit: Current = A, comment: str = ''):
-    """
-    Creates a differential current force operation.
-
-    Args:
-        signal (str): The primary signal to apply the force.
-        reference (str): The reference signal.
-        unit (Current): The unit of measurement (default is Amperes).
-        comment (str, optional): Optional comment for the operation. Defaults to ''.
-    """
+def DAFORCE(signal: str, reference: str, value: Union[int, float], unit: Current = A, comment: str = ''):
     g.output.append(CurrentForceOperation(unit=unit,
-                                           signal=signal,
-                                           reference=reference,
-                                           comment=comment))
+                                          value=value,
+                                          signal=signal,
+                                          reference=reference,
+                                          comment=comment))
 
-
-def RESFORCE(signal: str, unit: Resistance = Ohm, comment: str = ''):
-    """
-    Creates a resistance force operation.
-
-    Args:
-        signal (str): The signal to apply the force.
-        unit (Resistance): The unit of measurement (default is Ohms).
-        comment (str, optional): Optional comment for the operation. Defaults to ''.
-    """
+def RESFORCE(signal: str, value: Union[int, float], unit: Resistance = Ohm, comment: str = ''):
     g.output.append(ResistanceForceOperation(unit=unit,
-                                              signal=signal,
-                                              reference='GND',
-                                              comment=comment))
+                                             value=value,
+                                             signal=signal,
+                                             reference='GND',
+                                             comment=comment))
 
-
-def DRESFORCE(signal: str, reference: str, unit: Resistance = Ohm, comment: str = ''):
-    """
-    Creates a differential resistance force operation.
-
-    Args:
-        signal (str): The primary signal to apply the force.
-        reference (str): The reference signal.
-        unit (Resistance): The unit of measurement (default is Ohms).
-        comment (str, optional): Optional comment for the operation. Defaults to ''.
-    """
+def DRESFORCE(signal: str, reference: str, value: Union[int, float], unit: Resistance = Ohm, comment: str = ''):
     g.output.append(ResistanceForceOperation(unit=unit,
-                                              signal=signal,
-                                              reference=reference,
-                                              comment=comment))
+                                             value=value,
+                                             signal=signal,
+                                             reference=reference,
+                                             comment=comment))
 
-
-def CLKFORCE(signal: str, unit: Frequency = Hz, comment: str = ''):
-    """
-    Creates a clock force operation.
-
-    Args:
-        signal (str): The signal to apply the force.
-        unit (Frequency): The unit of measurement (default is Hertz).
-        comment (str, optional): Optional comment for the operation. Defaults to ''.
-    """
+def CLKFORCE(signal: str, value: Union[int, float], unit: Frequency = Hz, comment: str = ''):
     g.output.append(ClockForceOperation(unit=unit,
-                                         signal=signal,
-                                         reference='GND',
-                                         comment=comment))
+                                        value=value,
+                                        signal=signal,
+                                        reference='GND',
+                                        comment=comment))
 
-
-def DCLKFORCE(signal: str, reference: str, unit: Frequency = Hz, comment: str = ''):
-    """
-    Creates a differential clock force operation.
-
-    Args:
-        signal (str): The primary signal to apply the force.
-        reference (str): The reference signal.
-        unit (Frequency): The unit of measurement (default is Hertz).
-        comment (str, optional): Optional comment for the operation. Defaults to ''.
-    """
+def DCLKFORCE(signal: str, reference: str, value: Union[int, float], unit: Frequency = Hz, comment: str = ''):
     g.output.append(ClockForceOperation(unit=unit,
-                                         signal=signal,
-                                         reference=reference,
-                                         comment=comment))
+                                        value=value,
+                                        signal=signal,
+                                        reference=reference,
+                                        comment=comment))
--- src/dfttools/ops/forceops.py
+++ src/dfttools/ops/forceops.py
@@ -1,180 +1,200 @@
 import json
 from typing import Union
 from ..enum import Enum
-from . import TestOperation
 from . import Instructions
 
-class VoltageForceOperation(TestOperation):
-    def __init__(self, unit: Enum, signal: str,
+class ForceTestOperation(Enum):
+    FORCE = Instructions.FORCE
+
+    def __init__(self, t: int, signal1: str = None, signal2: str = None,
+                 comment: str = "", unit: Enum = None, value: Union[int, float] = None,
+                 variable: Enum = None, **kwargs):
+        """
+        Initializes a ForceTestOperation.
+
+        Args:
+            t (int): The type of test operation.
+            signal1 (str, optional): The primary signal. Defaults to None.
+            signal2 (str, optional): The secondary signal. Defaults to None.
+            comment (str, optional): A comment for the operation. Defaults to "".
+            unit (Enum, optional): The unit of measurement. Defaults to None.
+            value (Union[int, float], optional): The value to apply for the force operation. Defaults to None.
+            variable (Enum, optional): Additional variable for the operation. Defaults to None.
+            **kwargs: Additional keyword arguments.
+        """
+        self._unit = unit
+        self._comment = comment
+        self._signal1 = signal1
+        self._signal2 = signal2
+        self._value = value
+        self._variable = variable
+        super().__init__(t, zip(Instructions.values(), Instructions.keys()))
+
+    def to_dict(self) -> dict:
+        """
+        Returns the initialized values as a dictionary.
+
+        Returns:
+            dict: A dictionary representation of the initialized values.
+        """
+        return {
+            'type': 'FORCE',
+            'signal1': self._signal1,
+            'signal2': self._signal2,
+            'unit': str(self._unit) if self._unit else None,
+            'value': self._value,
+            'variable': str(self._variable) if self._variable else None,
+            'comment': self._comment
+        }
+
+    def to_json(self) -> str:
+        """
+        Returns the initialized values as a JSON string.
+
+        Returns:
+            str: A JSON string representation of the initialized values.
+        """
+        return json.dumps(self.to_dict())
+
+    def __repr__(self):
+        """
+        Returns a string representation of the ForceTestOperation.
+
+        Returns:
+            str: A string representation of the operation.
+        """
+        return f'Force Test Operation: {self._signal1} wrt {self._signal2}, ' \
+               f'value: {self._value}, unit: {self._unit}, variable: {self._variable}, ' \
+               f'{"( " + self._comment + " )" if self._comment else ""}'
+
+class VoltageForceOperation(ForceTestOperation):
+    def __init__(self, unit: Enum, signal: str, value: Union[float, int],
                  reference: str = 'GND', comment=None):
         """
         Initializes a VoltageForceOperation.
 
         Args:
-            unit (Enum): The unit of voltage force.
-            signal (str): The signal to force.
-            reference (str, optional): The reference signal. Defaults to 'GND'.
-            comment (str, optional): A comment for the operation. Defaults to None.
+            unit (Enum): The unit of voltage force
+            signal (str): The signal to force
+            value (Union[float, int]): Force value to apply
+            reference (str, optional): Reference signal. Defaults to 'GND'
+            comment (str, optional): Operation comment. Defaults to None
         """
         super().__init__(t=Instructions.FORCE, signal1=signal,
-                         signal2=reference, comment=comment, unit=unit)
+                         signal2=reference, comment=comment, 
+                         unit=unit, value=value)
 
     def __repr__(self):
         what = '' if self._signal2 == 'GND' else 'Differential '
-        return f'{what}voltage force: {self._signal1} wrt {self._signal2}, unit: {self._unit} { "( " + self._comment + " )" if self._comment is not None else ""}'
+        return f'{what}voltage force: {self._signal1} wrt {self._signal2}, ' \
+               f'value: {self._value} {self._unit} ' \
+               f'{"( " + self._comment + " )" if self._comment else ""}'
 
     def to_dict(self) -> dict:
-        """
-        Returns the initialized values as a dictionary.
-
-        Returns:
-            dict: A dictionary representation of the initialized values.
-        """
         return {
             'type': 'FORCE',
             'signal': self._signal1,
             'reference': self._signal2,
             'unit': str(self._unit),
+            'value': self._value,
             'comment': self._comment
         }
 
-    def to_json(self) -> str:
-        """
-        Returns the initialized values as a JSON string.
-
-        Returns:
-            str: A JSON string representation of the initialized values.
-        """
-        return json.dumps(self.to_dict())
-
-
-class CurrentForceOperation(TestOperation):
-    def __init__(self, unit: Enum, signal: str,
+class CurrentForceOperation(ForceTestOperation):
+    def __init__(self, unit: Enum, signal: str, value: Union[float, int],
                  reference: str = 'GND', comment=None):
         """
         Initializes a CurrentForceOperation.
 
         Args:
-            unit (Enum): The unit of current force.
-            signal (str): The signal to force.
-            reference (str, optional): The reference signal. Defaults to 'GND'.
-            comment (str, optional): A comment for the operation. Defaults to None.
+            unit (Enum): The unit of current force
+            signal (str): The signal to force
+            value (Union[float, int]): Force value to apply
+            reference (str, optional): Reference signal. Defaults to 'GND'
+            comment (str, optional): Operation comment. Defaults to None
         """
         super().__init__(t=Instructions.FORCE, signal1=signal,
-                         signal2=reference, comment=comment, unit=unit)
+                         signal2=reference, comment=comment, 
+                         unit=unit, value=value)
 
     def __repr__(self):
         what = '' if self._signal2 == 'GND' else 'Differential '
-        return f'{what}current force: {self._signal1} wrt {self._signal2}, unit: {self._unit} { "( " + self._comment + " )" if self._comment is not None else ""}'
+        return f'{what}current force: {self._signal1} wrt {self._signal2}, ' \
+               f'value: {self._value} {self._unit} ' \
+               f'{"( " + self._comment + " )" if self._comment else ""}'
 
     def to_dict(self) -> dict:
-        """
-        Returns the initialized values as a dictionary.
-
-        Returns:
-            dict: A dictionary representation of the initialized values.
-        """
         return {
             'type': 'FORCE',
             'signal': self._signal1,
             'reference': self._signal2,
             'unit': str(self._unit),
+            'value': self._value,
             'comment': self._comment
         }
 
-    def to_json(self) -> str:
-        """
-        Returns the initialized values as a JSON string.
-
-        Returns:
-            str: A JSON string representation of the initialized values.
-        """
-        return json.dumps(self.to_dict())
-
-
-class ResistanceForceOperation(TestOperation):
-    def __init__(self, unit: Enum, signal: str,
+class ResistanceForceOperation(ForceTestOperation):
+    def __init__(self, unit: Enum, signal: str, value: Union[float, int],
                  reference: str = 'GND', comment=None):
         """
         Initializes a ResistanceForceOperation.
 
         Args:
-            unit (Enum): The unit of resistance force.
-            signal (str): The signal to force.
-            reference (str, optional): The reference signal. Defaults to 'GND'.
-            comment (str, optional): A comment for the operation. Defaults to None.
+            unit (Enum): The unit of resistance force
+            signal (str): The signal to force
+            value (Union[float, int]): Force value to apply
+            reference (str, optional): Reference signal. Defaults to 'GND'
+            comment (str, optional): Operation comment. Defaults to None
         """
         super().__init__(t=Instructions.FORCE, signal1=signal,
-                         signal2=reference, comment=comment, unit=unit)
+                         signal2=reference, comment=comment, 
+                         unit=unit, value=value)
 
     def __repr__(self):
         what = '' if self._signal2 == 'GND' else 'Differential '
-        return f'{what}resistance force: {self._signal1} wrt {self._signal2}, unit: {self._unit} { "( " + self._comment + " )" if self._comment is not None else ""}'
+        return f'{what}resistance force: {self._signal1} wrt {self._signal2}, ' \
+               f'value: {self._value} {self._unit} ' \
+               f'{"( " + self._comment + " )" if self._comment else ""}'
 
     def to_dict(self) -> dict:
-        """
-        Returns the initialized values as a dictionary.
-
-        Returns:
-            dict: A dictionary representation of the initialized values.
-        """
         return {
             'type': 'FORCE',
             'signal': self._signal1,
             'reference': self._signal2,
             'unit': str(self._unit),
+            'value': self._value,
             'comment': self._comment
         }
 
-    def to_json(self) -> str:
-        """
-        Returns the initialized values as a JSON string.
-
-        Returns:
-            str: A JSON string representation of the initialized values.
-        """
-        return json.dumps(self.to_dict())
-
-
-class ClockForceOperation(TestOperation):
-    def __init__(self, unit: Enum, signal: str,
+class ClockForceOperation(ForceTestOperation):
+    def __init__(self, unit: Enum, signal: str, value: Union[float, int],
                  reference: str = 'GND', comment=None):
         """
         Initializes a ClockForceOperation.
 
         Args:
-            unit (Enum): The unit of clock frequency.
-            signal (str): The signal to force.
-            reference (str, optional): The reference signal. Defaults to 'GND'.
-            comment (str, optional): A comment for the operation. Defaults to None.
+            unit (Enum): The unit of clock frequency
+            signal (str): The signal to force
+            value (Union[float, int]): Force value to apply
+            reference (str, optional): Reference signal. Defaults to 'GND'
+            comment (str, optional): Operation comment. Defaults to None
         """
         super().__init__(t=Instructions.FORCE, signal1=signal,
-                         signal2=reference, comment=comment, unit=unit)
+                         signal2=reference, comment=comment, 
+                         unit=unit, value=value)
 
     def __repr__(self):
         what = '' if self._signal2 == 'GND' else 'Differential '
-        return f'{what}clock force: {self._signal1} wrt {self._signal2}, unit: {self._unit} { "( " + self._comment + " )" if self._comment is not None else ""}'
+        return f'{what}clock force: {self._signal1} wrt {self._signal2}, ' \
+               f'value: {self._value} {self._unit} ' \
+               f'{"( " + self._comment + " )" if self._comment else ""}'
 
     def to_dict(self) -> dict:
-        """
-        Returns the initialized values as a dictionary.
-
-        Returns:
-            dict: A dictionary representation of the initialized values.
-        """
         return {
             'type': 'FORCE',
             'signal': self._signal1,
             'reference': self._signal2,
             'unit': str(self._unit),
+            'value': self._value,
             'comment': self._comment
         }
-
-    def to_json(self) -> str:
-        """
-        Returns the initialized values as a JSON string.
-
-        Returns:
-            str: A JSON string representation of the initialized values.
-        """
-        return json.dumps(self.to_dict())
