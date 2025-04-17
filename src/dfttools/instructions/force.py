f78835279c11a8e8dc0c97564440ee64ee486497
unknown <harishkumarsedu@gmail.com>
2025-04-17 13:40:29 +0200

    trigger imported in init

--- src/dfttools/instructions/force.py
+++ src/dfttools/instructions/force.py
@@ -4,7 +4,7 @@ from dfttools.hardware.force import apply_force_and_measure
 def VFORCE(signal: str = 'VCC', reference: str = 'GND', value: float = 0.0):
     harware_availabel,measured_value = apply_force_and_measure(g, signal, reference, value, 'voltage_force')
     if not  harware_availabel:
-        return {'signal': signal, 'reference': reference, 'value': value}
+        return value
     
     g.output.append({'type': 'FORCE', 'signal': signal, 'reference': reference, 'value': value})
     return measured_value
@@ -12,7 +12,7 @@ def VFORCE(signal: str = 'VCC', reference: str = 'GND', value: float = 0.0):
 def AFORCE(signal: str = 'VCC', reference: str = 'GND', value: float = 0.0):
     hardware_available, measured_value = apply_force_and_measure(g, signal, reference, value, 'current_force')
     if not hardware_available:
-        return {'signal': signal, 'reference': reference, 'value': value}
+        return value
     
     g.output.append({'type': 'FORCE', 'signal': signal, 'reference': reference, 'value': value})
     return measured_value
@@ -20,7 +20,7 @@ def AFORCE(signal: str = 'VCC', reference: str = 'GND', value: float = 0.0):
 def RESFORCE(signal: str = 'VCC', reference: str = 'GND', value: float = 0.0):
     hardware_available, measured_value = apply_force_and_measure(g, signal, reference, value, 'resistance_force')
     if not hardware_available:
-        return {'signal': signal, 'reference': reference, 'value': value}
+        return value
     
     g.output.append({'type': 'FORCE', 'signal': signal, 'reference': reference, 'value': value})
     return measured_value
@@ -28,7 +28,7 @@ def RESFORCE(signal: str = 'VCC', reference: str = 'GND', value: float = 0.0):
 def FREQFORCE(signal: str = 'VCC', reference: str = 'GND', value: float = 0.0):
     hardware_available, measured_value = apply_force_and_measure(g, signal, reference, value, 'frequency_force')
     if not hardware_available:
-        return {'signal': signal, 'reference': reference, 'value': value}
+        return value
     
     g.output.append({'type': 'FORCE', 'signal': signal, 'reference': reference, 'value': value})
     return measured_value
