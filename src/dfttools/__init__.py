75881eaa0bfc143af3fca99d26d2194711ee1e20
unknown <harishkumarsedu@gmail.com>
2025-04-17 13:18:56 +0200

    trigger imported in init

--- src/dfttools/__init__.py
+++ src/dfttools/__init__.py
@@ -2,21 +2,24 @@ from dfttools.instructions.force import *
 from dfttools.instructions.meas import *
 from dfttools.instructions.force_sweep import *
 from dfttools.instructions.I2C import *
+from dfttools.instructions.trigger import *
 from dfttools.glob import g
 from dfttools.callbacks.force_callbacks import *
 from dfttools.callbacks.measure_callbacks import *
 from dfttools.callbacks.force_sweep_callbacks import *
 from dfttools.callbacks.i2c_callback import *
+from dfttools.callbacks.trig_callbacks import *
+from dfttools.callbacks.trig_custom_callbacks import *
 
 import importlib
 import inspect
 
 __all__ = [name for name in dir() if not name.startswith("_")]
 
-module = importlib.import_module("dfttools")  # Pass the module name as a string
+# module = importlib.import_module("dfttools")  # Pass the module name as a string
         
 # Unified list for all suggestions
-suggestions = set()
+# suggestions = set()
 
 # # Get functions with their definitions (signatures and docstrings)
 # for name, obj in inspect.getmembers(module, inspect.isfunction):
@@ -24,6 +27,6 @@ suggestions = set()
 #     suggestions.add(f"{name}{func_signature}")
 
 # Get keywords (includes attributes, classes, etc.)
-for keyword in dir(module):
-    suggestions.add(f"{keyword}")
-print(suggestions)
+# for keyword in dir(module):
+#     suggestions.add(f"{keyword}")
+# print(suggestions)
