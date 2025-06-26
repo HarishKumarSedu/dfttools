3bbf8c171eaf38df6bec6e03b91c4bf0a58718a0
Harish Kumar Shivaramappa <harishkumarsedu@gmail.com>
2025-06-26 13:48:20 +0200

    exprected value remvoed from emasure instructions to pass it to callback functions

--- tests/measure_testing.py
+++
@@ -1,7 +0,0 @@
-from dfttools import *
-def frequency_measure(*args,**kwargs):
-  print(args,kwargs)
-  return False,False
-g.hardware_callbacks['frequency_measure'] = frequency_measure
-
-print("Frequency Measurement:", FREQMEASURE(signal='CLK', reference='GND', expected_value=60))
